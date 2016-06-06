4e83afd7deec39ef1bc8e7d5b06e7b648a17f26a
Logan <loganskidmore91@gmail.com>
2016-06-06 09:50:32 -0600

    Character Page

---
+++ app/src/main/java/com/example/logan/goaldigger/CharacterPage.java
@@ -0,0 +1,13 @@
+package com.example.logan.goaldigger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CharacterPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_page);
+    }
+}
