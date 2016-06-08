b6bc02fdbec4a5a848c98a057133dd69c238f328
Logan <loganskidmore91@gmail.com>
2016-06-08 10:01:56 -0600

    Character Page added tests

--- app/src/main/java/com/example/logan/goaldigger/CharacterPage.java
+++ app/src/main/java/com/example/logan/goaldigger/CharacterPage.java
@@ -11,3 +11,20 @@ public class CharacterPage extends AppCompatActivity {
         setContentView(R.layout.activity_character_page);
     }
 }
+
+class Character {
+        int level = 0;
+        double experience = 0;
+        String name = "";
+        void testAttributes(){
+            if(level != 0 && experience != 0 && name != "") {
+                //everything seems A-OK
+            }
+            else {
+                throw new AssertionError("The attributes did not get received");
+            }
+        }
+
+
+
+}
---
+++ app/src/main/java/com/example/logan/goaldigger/goalPage.java
@@ -0,0 +1,24 @@
+package com.example.logan.goaldigger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class goalPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goal_page);
+    }
+}
+
+class Goal{
+    int start;
+    int end;
+    String description;
+    void testDates(){
+        if(start > end){
+            throw new AssertionError("The end date is before start date");
+        }
+    }
+}
