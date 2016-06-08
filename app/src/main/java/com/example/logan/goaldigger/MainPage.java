14a31b0bb9f7a4a6a2d06df346dff3595eb908dd
John Krieger <smockmanzero@gmail.com>
2016-06-08 10:02:57 -0600

    Added a test to make sure a user exists when it logs in a and a test to make sure a user name is not already taken. Go Browns!!

--- app/src/main/java/com/example/logan/goaldigger/MainPage.java
+++ app/src/main/java/com/example/logan/goaldigger/MainPage.java
@@ -2,6 +2,8 @@ package com.example.logan.goaldigger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainPage extends AppCompatActivity {
 
@@ -10,4 +12,43 @@ public class MainPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
     }
+
+   class User{
+        boolean loggedIn = true;
+        List<User> userList = new ArrayList<User>();
+        String userName;
+        String passWord;
+       void logIn(){
+
+
+
+    loggedIn = true;
+
+       }
+
+
+       boolean doesUserExist(User aUser) {
+           for (int i = 0; i < userList.size(); i++) {
+            if(aUser == userList.get(i)) {
+                return true;
+            }
+            else
+                return false;
+           }
+           throw new AssertionError();
+       }
+
+       boolean doesUsernameExist(User aUser){
+           for (int i = 0; i < userList.size(); i++) {
+               if (aUser.userName == userList.get(i).userName) {
+                    return true;
+               }
+               else
+                   return false;
+           }throw new AssertionError();
+       }
+
+    }
+
+
 }
