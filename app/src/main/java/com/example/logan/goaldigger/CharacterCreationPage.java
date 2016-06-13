18cb21897a206271e33a77df04047f2da1654e31
John Krieger <smockmanzero@gmail.com>
2016-06-13 09:39:37 -0600

    Added the Character creation page

---
+++ app/src/main/java/com/example/logan/goaldigger/CharacterCreationPage.java
@@ -0,0 +1,13 @@
+package com.example.logan.goaldigger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CharacterCreationPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_creation_page);
+    }
+}
