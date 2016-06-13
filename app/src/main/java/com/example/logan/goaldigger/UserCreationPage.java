b5bd24f710ae51bb4d7efb4f4e913633371aa1b8
Logan <loganskidmore91@gmail.com>
2016-06-13 09:42:54 -0600

    Character Creation page

---
+++ app/src/main/java/com/example/logan/goaldigger/UserCreationPage.java
@@ -0,0 +1,13 @@
+package com.example.logan.goaldigger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserCreationPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_creation_page);
+    }
+}
