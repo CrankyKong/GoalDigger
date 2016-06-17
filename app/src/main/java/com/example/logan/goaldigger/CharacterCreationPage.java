d802484ce34bba5efaa7ec0f11b949e29132a013
Logan <loganskidmore91@gmail.com>
2016-06-17 16:08:41 -0600

    Logan messing with character creation

--- app/src/main/java/com/example/logan/goaldigger/CharacterCreationPage.java
+++ app/src/main/java/com/example/logan/goaldigger/CharacterCreationPage.java
@@ -2,19 +2,31 @@ package com.example.logan.goaldigger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CharacterCreationPage extends AppCompatActivity {
+
+    Button mButton;
+    EditText mEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_creation_page);
-    }
+        mButton = (Button)findViewById(R.id.avatarCreateButton);
+        mEdit   = (EditText)findViewById(R.id.avatarName);
 
-    void createCharacter(){
-
-        
-
+        mButton.setOnClickListener(
+                new View.OnClickListener()
+                {
+                    public void onClick(View view)
+                    {
+                        Log.v("EditText", mEdit.getText().toString());
+                    }
+                });
 
     }
 
