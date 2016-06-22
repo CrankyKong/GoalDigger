6cc547b05a290a7f6dcd5e6a73c314ab7de203fc
John Krieger <smockmanzero@gmail.com>
2016-06-22 15:48:16 -0600

    Logan is a Girl/ Added log entires for userCreation

--- app/src/main/java/com/example/logan/goaldigger/database.java
+++ app/src/main/java/com/example/logan/goaldigger/database.java
@@ -1,7 +1,32 @@
 package com.example.logan.goaldigger;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by John  Krieger on 6/22/2016.
  */
-public class database {
+public class database extends SQLiteOpenHelper {
+
+
+
+    public database(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        SQLiteDatabase goalDiggerDB = SQLiteDatabase.openOrCreateDatabase("goalDigger",null );
+        goalDiggerDB.execSQL("CREATE TABLE USER ");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+
+
+
 }
